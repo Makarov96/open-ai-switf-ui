02b5a3cf9a4d64208b9d17d2e3ef020dd1285c97
Makarov96 <estev97@gmail.com>
2023-05-25 18:50:15 -0600

    Initial Commit

---
+++ openai/openaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  openaiApp.swift
+//  openai
+//
+//  Created by Guerin Steven Colocho Chacon on 25/05/23.
+//
+
+import SwiftUI
+
+@main
+struct openaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
